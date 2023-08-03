5dc6f9bee0e0471dfac8d44015aacae6e9e2d439
Level222 <124219660+Level222@users.noreply.github.com>
2023-08-03 15:43:16 +0900

    init

---
+++ README.md
@@ -0,0 +1,124 @@
+# Bookmarklet Output Webpack Plugin
+
+Webpack Plugin to output JavaScript in bookmarklet format.
+
+## Installation
+
+```shell
+npm i -D bookmarklet-output-webpack-plugin
+```
+
+## Usage
+
+```javascript
+// webpack.config.js
+
+const path = require('path');
+const BookmarkletOutputWebpackPlugin = require("bookmarklet-output-webpack-plugin");
+
+module.exports = {
+  // ...
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "example-bookmarklet.js"
+  },
+  plugins: [
+    new BookmarkletOutputWebpackPlugin()
+  ]
+};
+```
+
+```javascript
+// dist/example-bookmarklet.js
+
+javascript:alert(%22Hello%22)%3B
+```
+
+### Output as a New File
+
+```javascript
+new BookmarkletOutputWebpackPlugin({
+  newFile: true
+})
+```
+
+```javascript
+// dist/example-bookmarklet.js
+
+alert("Hello");
+```
+
+```javascript
+// dist/example-bookmarklet.bookmarklet.js
+
+javascript:alert(%22Hello%22)%3B
+```
+
+### Create HTML File of the List of Bookmarklets
+
+```javascript
+new BookmarkletOutputWebpackPlugin({
+  bookmarkletsList: true
+})
+```
+
+```javascript
+// dist/example-bookmarklet.js
+
+javascript:alert(%22Hello%22)%3B
+```
+
+```html
+<!-- dist/bookmarklets.html -->
+
+<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Bookmarklets</title><style>body{font:18px sans-serif;margin:20px}</style></head><body><p>You can drag the following bookmarklets and register for the bookmark.</p><ul><li><a href="javascript:alert(%22Hello%22)%3B">test.js</a></li></ul></body></html>
+```
+
+## Options
+
+```typescript
+{
+  urlEncode?: boolean;
+  // default: true
+  // URL encoding.
+
+  include?: RegExp;
+  // default: /\.js$/
+  // Regular expression for filenames to include.
+
+  newFile?: boolean;
+  // default: false
+  // Output as a new file.
+
+  newFileName?: string;
+  // default: "[path][name].bookmarklet[ext]"
+  // Name of the new output file.
+  // "[path]", "[name]", and "[ext]" will be replaced.
+
+  bookmarkletsList?: boolean;
+  // default: false
+  // Output HTML file of the list of bookmarklets.
+
+  bookmarkletsListName?: string;
+  // default: "bookmarklets.html"
+  // File name of the bookmarklets list.
+
+  removeEntryFile?: boolean;
+  // default: false
+  // Remove entry js file.
+  // Use with bookmarkletsList and output only bookmarklets list.
+
+  createBookmarkletsList?: (bookmarklets: { filename: string; bookmarklet: string; }[]) => string;
+  // default: (bookmarklets) => { /* ... */ }
+  // Function to create a bookmarklets list.
+  // You can customize bookmarklets list with this option.
+}
+```
+
+## Note
+
+This plugin does not support `webpack-dev-server`.
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,131 @@
+import path from "path";
+import type { WebpackPluginInstance, Compiler } from "webpack";
+
+type Bookmarklet = {
+  filename: string;
+  bookmarklet: string;
+};
+
+type Options = {
+  urlEncode: boolean;
+  include: RegExp;
+  newFile: boolean;
+  newFileName: string;
+  bookmarkletsList: boolean;
+  bookmarkletsListName: string;
+  removeEntryFile: boolean;
+  createBookmarkletsList: (bookmarklets: Bookmarklet[]) => string;
+};
+
+const escapeHtml = (str: string): string => {
+  return str
+    .replaceAll("&", "&amp;")
+    .replaceAll('"', "&#34;")
+    .replaceAll("'", "&#39;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;");
+};
+
+class BookmarkletOutputWebpackPlugin implements WebpackPluginInstance {
+  static defaultOptions: Options = {
+    urlEncode: true,
+    include: /\.js$/,
+    newFile: false,
+    newFileName: "[path][name].bookmarklet[ext]",
+    bookmarkletsList: false,
+    bookmarkletsListName: "bookmarklets.html",
+    removeEntryFile: false,
+    createBookmarkletsList: (bookmarklets: Bookmarklet[]): string => {
+      const bookmarkletsListItems = bookmarklets.map(({ filename, bookmarklet }) => (
+        `<li><a href="${escapeHtml(bookmarklet)}">${escapeHtml(filename)}</a></li>`
+      ));
+
+      return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content=width=device-width, initial-scale=1.0">
+          <title>Bookmarklets</title>
+          <style>body{font:18px sans-serif;margin:20px}</style>
+        </head>
+        <body>
+          <p>You can drag the following bookmarklets and register for the bookmark.</p>
+          <ul>${bookmarkletsListItems.join("")}</ul>
+        </body>
+        </html>
+      `.replace(/^\s+|\s+$|\n/mg, "");
+    }
+  };
+
+  options: Options;
+
+  constructor(options?: Partial<Options>) {
+    this.options = {
+      ...BookmarkletOutputWebpackPlugin.defaultOptions,
+      ...options
+    };
+  }
+
+  apply(compiler: Compiler): void {
+    const pluginName = BookmarkletOutputWebpackPlugin.name;
+
+    const {
+      Compilation,
+      sources: { RawSource }
+    } = compiler.webpack;
+
+    compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
+      compilation.hooks.processAssets.tap(
+        {
+          name: pluginName,
+          stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE - 1
+        },
+        (assets) => {
+          const targetAssets = Object.entries(assets).filter(([filename]) => (
+            this.options.include.test(filename)
+          ));
+
+          const bookmarklets: Bookmarklet[] = targetAssets.map(([filename, asset]) => {
+            const code = asset.source();
+
+            if (typeof code !== "string") {
+              throw new TypeError(`${filename} is not a text file.`);
+            }
+
+            const bookmarklet = `javascript:${this.options.urlEncode ? encodeURIComponent(code) : code}`;
+            return { filename, bookmarklet };
+          });
+
+          for (const { filename, bookmarklet } of bookmarklets) {
+            const newSource = new RawSource(bookmarklet);
+
+            if (this.options.newFile) {
+              const parsedPath = path.parse(filename);
+              const newFileName = this.options.newFileName
+                .replaceAll("[path]", `${parsedPath.dir}/`)
+                .replaceAll("[name]", parsedPath.name)
+                .replaceAll("[ext]", parsedPath.ext);
+              compilation.emitAsset(newFileName, newSource, {
+                minimized: true
+              });
+            } else {
+              compilation.updateAsset(filename, newSource);
+            }
+
+            if (this.options.removeEntryFile) {
+              compilation.deleteAsset(filename);
+            }
+          }
+
+          if (this.options.bookmarkletsList) {
+            const bookmarkletsList = this.options.createBookmarkletsList(bookmarklets);
+            const bookmarkletsListSource = new RawSource(bookmarkletsList);
+            compilation.emitAsset(this.options.bookmarkletsListName, bookmarkletsListSource);
+          }
+        });
+    });
+  }
+};
+
+export = BookmarkletOutputWebpackPlugin;
